4c07bd1f319c7483bb8b8cb48dac913be2d0d362
luis50019 <teddiazdiaz019@gmail.com>
2024-12-09 21:26:44 -0600

    cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'0.0.0.0',
-    cors:true,
-    port:5173,
-    hmr:false
-  }
 })
